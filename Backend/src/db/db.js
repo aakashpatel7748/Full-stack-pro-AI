834df6fdc25c0dab07ed7e9487a01f8561f6888d
Aakash Patel <apatel7748@gmail.com>
2026-02-03 04:32:27 +0530

    Fix Mongoose connection options

--- Backend/src/db/db.js
+++ Backend/src/db/db.js
@@ -1,18 +1,15 @@
 import mongoose from "mongoose";
 import config from "../config/config.js";
 
-function connectDB(){
-    mongoose.connect(config.MONGODB_URI,{
-        useNewUrlParser:true,
-        useUnifiedTopology:true    
-    })
+function connectDB() {
+    mongoose.connect(config.MONGODB_URI)
 
-    .then(()=>{
-        console.log("MongoDB Connected")
-    })
-    .catch((err)=>{
-        console.error("MongoDB Connection error", err)
-    })
+        .then(() => {
+            console.log("MongoDB Connected")
+        })
+        .catch((err) => {
+            console.error("MongoDB Connection error", err)
+        })
 }
 
 export default connectDB
